4b67eac69eee8146eaeb3a2958afabbd009d032b
TosineBadges <tosinebadges@gmail.com>
2024-01-12 15:09:52 -0800

    Executable files

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Bajomo Oluwatosin <https://github.com/TosineBadges>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
